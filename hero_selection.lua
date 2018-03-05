f909a95b9eb10f4403b54832d151dece1f7f1c25
yuniyuni <yunizhan@usc.edu>
2018-03-04 17:57:15 -0800

    optimize hero selection alg

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,53 +1,26 @@
 
-
 ----------------------------------------------------------------------------------------------------
 
-hero_pool_my=
-{
-		"npc_dota_hero_lina",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana",
-		"npc_dota_hero_mirana"
-}
-        
 function Think()
-	for i,id in pairs(GetTeamPlayers(GetTeam())) 
-	do
-		if ( GetTeam() == TEAM_RADIANT )
-		then
-				if(IsPlayerBot(id) and (GetSelectedHeroName(id)=="" or GetSelectedHeroName(id)==nil))
-				then
-					local num= hero_pool_my[2] 		--取随机数
-					SelectHero( id, num );			--在保存英雄名称的表中，随机选择出AI的英雄
-					table.remove(hero_pool_my,2)		--移除这个英雄
-				end
-		elseif ( GetTeam() == TEAM_DIRE )
-		then
-				if(IsPlayerBot(id) and (GetSelectedHeroName(id)=="" or GetSelectedHeroName(id)==nil))
-				then
-					local num= hero_pool_my[1] 		--取随机数
-					SelectHero( id, num );			--在保存英雄名称的表中，随机选择出AI的英雄
-					table.remove(hero_pool_my,1)		--移除这个英雄
-				end
-		end	
-	end
+	hasLina = false
+ 	for i,id in pairs(GetTeamPlayers(GetTeam())) do
+        if (IsPlayerBot(id)) and (GetSelectedHeroName(id)=="" or GetSelectedHeroName(id)==nil) then
+        	if (GetTeam() == TEAM_RADIANT and hasLina == false) then
+            	SelectHero( id, "npc_dota_hero_lina" )
+            	hasLina = true
+            else 
+            	SelectHero( id, "npc_dota_hero_mirana" )
+            end
+        end
+    end
+
 end
 
 function UpdateLaneAssignments()
 
 	local lanes=
 	{
-		[1]=LANE_TOP,
+		[1]=LANE_MID,
 		[2]=LANE_TOP,
 		[3]=LANE_TOP,
 		[4]=LANE_TOP,
